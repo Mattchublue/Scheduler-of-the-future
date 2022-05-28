ce1f7531f587409dc66c72079012d4fae571a674
Matthew <mwhiteman83@gmail.com>
2022-05-28 17:39:35 -0600

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,9 @@ function updateTime() {
 $("#currentDay").text(today.format("MMMM Do YYYY"))
 };
 updateTime()
+
+$(".save-icon").on("click",function(){
+    let textValue = $(this).siblings(".text-value").val()
+    let divNumber = $(this).parent().attr("id")
+    localStorage.setItem(divNumber,textValue)
+})
